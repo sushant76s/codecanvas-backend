900273063e34d217e6e318afed333157f295cafb
Sushant Verma <sushantverma9648@gmail.com>
2024-04-07 18:48:14 +0530

    fix: controllers

--- routes/healthCheck.js
+++ routes/healthCheck.js
@@ -1,15 +1,7 @@
 const express = require('express');
+const { healthCheck } = require('../controllers/healthCheckController');
 const router = express.Router();
 
-
-// server condition check
-router.get("/server-check", (req, res) => {
-    try {
-        res.status(200).json({ status: true, message: "server is running!" });
-    } catch (error) {
-        console.error("Something went wrong!");
-        res.status(500).json({ status: false, message: "oops server is not running :(" });
-    }
-});
+router.route("/server-check").get(healthCheck)
 
 module.exports = router;
